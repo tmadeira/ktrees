35e93647dc68a7971c23cef567e41401e6e8b5b1
Tiago Madeira <tmadeira@gmail.com>
2016-06-09 20:44:11 -0300

    Add TODO to characteristic package

--- src/characteristic/characteristic.go
+++ src/characteristic/characteristic.go
@@ -6,3 +6,6 @@ type Tree struct {
 }
 
 const Epsilon = -1
+
+// TODO: Implement Step 2 from Coding Algorithm (R_k -> char. tree).
+// TODO: Implement Step 3 from Decoding Algorithm (char. tree -> R_k).

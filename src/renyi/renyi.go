16826267624baa29f6c4cdfb9902a7e4e0b032dd
Tiago Madeira <tmadeira@gmail.com>
2016-06-09 20:44:21 -0300

    Add renyi package with TODO comments

---
+++ src/renyi/renyi.go
@@ -0,0 +1,5 @@
+// Package renyi implements the Renyi k-tree.
+package renyi
+
+// TODO: Implement Step 1 from Coding Algorithm (k-tree -> R_k).
+// TODO: Implement Step 4 from Decoding Algorithm (R_k -> k-tree).

4ac4e3fc90f040a7a9e6d3e4d82e726d052df1f8
Tiago Madeira <tmadeira@gmail.com>
2016-06-09 21:36:58 -0300

    Implement step 1 from Coding Algorithm (TODO: test)

--- src/renyi/renyi.go
+++ src/renyi/renyi.go
@@ -1,5 +1,26 @@
 // Package renyi implements the Renyi k-tree.
 package renyi
 
-// TODO: Implement Step 1 from Coding Algorithm (k-tree -> R_k).
+import (
+	"ktree"
+)
+
 // TODO: Implement Step 4 from Decoding Algorithm (R_k -> k-tree).
+
+type RenyiKtree struct {
+	Ktree *ktree.Ktree
+	Q     []int
+}
+
+// FromTk returns a Renyi k-Tree from a given k-Tree.
+// See Step 1 from Coding Algorithm in Section 5 of Caminiti et al.
+func FromTk(t *ktree.Ktree) (*RenyiKtree, error) {
+	Q, err := ktree.GetQ(t)
+	if err != nil {
+		return nil, err
+	}
+
+	phi := ktree.ComputePhi(len(t.Adj), t.K, Q)
+
+	return &RenyiKtree{ktree.Relabel(t, phi), Q}, nil
+}
